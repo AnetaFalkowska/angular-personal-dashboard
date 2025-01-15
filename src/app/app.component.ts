4674193c9cff3bfbc8128c17c6fdffbc62538fb5
Aneta Falkowska <aneta.falkowska@gmail.com>
2025-01-15 15:16:12 +0100

    display date and time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabsComponent } from './tabs/tabs.component';
+
 
 import {
   animate,
@@ -11,10 +12,20 @@ import {
   trigger,
 } from '@angular/animations';
 import { NotificationComponent } from './notification/notification.component';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { map, Observable, timer } from 'rxjs';
+
+const baseStyles = style({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+})
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TabsComponent, NotificationComponent],
+  imports: [RouterOutlet, TabsComponent, NotificationComponent, DatePipe, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   animations: [
@@ -24,13 +35,7 @@ import { NotificationComponent } from './notification/notification.component';
         query(
           ':enter, :leave',
           [
-            style({
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-            }),
+            baseStyles,
           ],
           { optional: true }
         ),
@@ -68,13 +73,7 @@ import { NotificationComponent } from './notification/notification.component';
         query(
           ':enter, :leave',
           [
-            style({
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-            }),
+            baseStyles,
           ],
           { optional: true }
         ),
@@ -107,6 +106,90 @@ import { NotificationComponent } from './notification/notification.component';
           ),
         ]),
       ]),
+      transition('*=>secondary', [
+        style({
+           position: 'relative', 
+          // overflow can't be hidden, otherwise only content of the box will sacle down, while the whole box is the same size during the animation
+          //  overflow: 'hidden' 
+          }),
+        query(
+          ':enter, :leave',
+          [
+            baseStyles,
+          ],
+          { optional: true }
+        ),
+        query(':enter', [style({ opacity: 0 })], { optional: true }),
+        group([
+          query(
+            ':leave',
+            [
+              animate(
+                '200ms ease-in',
+                style({ opacity: 0, transform: 'scale(0.8)' })
+              ),
+            ],
+            {
+              optional: true,
+            }
+          ),
+          query(
+            ':enter',
+            [
+              style({ transform: 'scale(1.2)' }),
+              animate(
+                '250ms 120ms ease-out',
+                style({ opacity: 1, transform: 'scale(1)' })
+              ),
+            ],
+            {
+              optional: true,
+            }
+          ),
+        ]),
+      ]),
+      transition('secondary=>*', [
+        style({
+           position: 'relative', 
+          // overflow can't be hidden, otherwise only content of the box will sacle down, while the whole box is the same size during the animation
+          //  overflow: 'hidden' 
+          }),
+        query(
+          ':enter, :leave',
+          [
+            baseStyles,
+          ],
+          { optional: true }
+        ),
+        query(':enter', [style({ opacity: 0 })], { optional: true }),
+        group([
+          query(
+            ':leave',
+            [
+              animate(
+                '200ms ease-in',
+                style({ opacity: 0, transform: 'scale(1.25)' })
+              ),
+            ],
+            {
+              optional: true,
+            }
+          ),
+          query(
+            ':enter',
+            [
+              style({ transform: 'scale(0.8)' }),
+              animate(
+                '250ms 120ms ease-out',
+                style({ opacity: 1, transform: 'scale(1)' })
+              ),
+            ],
+            {
+              optional: true,
+            }
+          ),
+        ]),
+      ]),
     ]),
   ],
   // animations: [
@@ -122,12 +205,28 @@ import { NotificationComponent } from './notification/notification.component';
   //   ]),
   // ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+//   dateTime!:Date
+
+// ngOnInit(): void {
+//   timer(0,1000).subscribe(()=> this.dateTime = new Date())
+// }
+
+
+dateTime!: Observable<Date>
+
+ngOnInit() {
+  this.dateTime = timer(0,1000).pipe(map(()=> {return new Date()}))
+}
+
+
+
   prepareRoute(outlet: RouterOutlet) {
     if (outlet.isActivated) {
-      const tab = outlet.activatedRouteData['tab']
-      if (!tab) return "secondary";
-      return tab
+      const tab = outlet.activatedRouteData['tab'];
+      if (!tab) return 'secondary';
+      return tab;
     }
   }
 }
